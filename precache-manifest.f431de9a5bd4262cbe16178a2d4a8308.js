b16e4523aa1d2bb8d17174c7bf4fa87a8e22542f
Paweł Pyc <pawelpyc154@gmail.com>
2019-11-29 18:37:46 +0100

    Updates

---
+++ precache-manifest.f431de9a5bd4262cbe16178a2d4a8308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4641f28a8b79c02936267c2d52af9232",
+    "url": "/TypeScript-Redux-Example-project-5/index.html"
+  },
+  {
+    "revision": "5918bc659d2d777cc439",
+    "url": "/TypeScript-Redux-Example-project-5/static/css/main.ca251178.chunk.css"
+  },
+  {
+    "revision": "1724aecb3a3e27751607",
+    "url": "/TypeScript-Redux-Example-project-5/static/js/2.f31d737b.chunk.js"
+  },
+  {
+    "revision": "5918bc659d2d777cc439",
+    "url": "/TypeScript-Redux-Example-project-5/static/js/main.004b5f23.chunk.js"
+  },
+  {
+    "revision": "99969713c1c9145c2bab",
+    "url": "/TypeScript-Redux-Example-project-5/static/js/runtime-main.11471d89.js"
+  }
+]);
